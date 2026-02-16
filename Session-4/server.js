97b18886c7bb99e03cc22cdb4f06a5ccd1083458
Zignuts Technolab <ztlab82@Zignutss-Mac-mini-2.local>
2026-02-16 19:01:36 +0530

    session 4

---
+++ Session-4/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const http = require('http')
+const server = http.createServer(app);
+const {Server} = require('socket.io');
+const io = new Server(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on('message', (data) => {
+        console.log("client says" , data);   
+    });
+    socket.on('typing', (data) => {
+        console.log("client is typing" , data);   
+    });
+    socket.emit('reply', 'Hello from server');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+});
+
+server.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
